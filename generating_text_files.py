4e0ca92eb57ee6af6a991005340349fee343e2ad
zarex1111 <lyubomirshvetsov@yandex.ru>
2022-12-15 20:27:40 +0200

    Первый коммит

---
+++ generating_text_files.py
@@ -0,0 +1,28 @@
+import pygame
+from settings import *
+from pygame import mixer_music
+
+
+if __name__ == '__main__':
+    filename = input()
+    with open(f'data/txt/{filename}.txt', 'w') as file:
+        file.writelines('0' * 8 for _ in range(ACTIVE_ROWS))
+        pygame.init()
+
+        running = True
+        clock = pygame.time.Clock()
+        keys = (pygame.K_d, pygame.K_w, pygame.K_a, pygame.K_s,
+            pygame.K_RIGHT, pygame.K_UP, pygame.K_LEFT, pygame.K_DOWN)
+        scene_number = 1
+
+        mixer_music.load(f'data/music/{filename}.wav')
+        mixer_music.play()
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            if mixer_music.get_busy():
+                pressed_keys = pygame.key.get_pressed()
+                line = list(map(lambda x: x in pressed_keys, keys))
+                print(line)
